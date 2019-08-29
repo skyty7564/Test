7c19cb646476d9b82d505bb0100fae07e2c07051
Bao Nguyen <Bao Nguyen@DESKTOP-OCHAU49>
2019-08-28 23:36:28 -0400

    Add project files.

---
+++ SportStore/Models/FakeProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace SportStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+
+        public IEnumerable<Product> Product => new List<Product>
+        {
+            new Product {Name = "Football", Price = 25},
+            new Product {Name = "Surf board", Price = 179},
+            new Product {Name = "Running Shoes", Price = 95}
+
+        };
+    }
+}
